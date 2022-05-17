279955fe8337a9c64883fd5e8b4df34456962185
aightech <aiiight.tech@gmail.com>
2022-05-17 13:13:23 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -5,116 +5,82 @@
 #include <stdlib.h>
 
 snd_pcm_t *
-init(snd_pcm_stream_t mode)
+init(snd_pcm_stream_t mode,  unsigned int actualRate)
 {
     int err;
     snd_pcm_t *playback_handle;
     snd_pcm_hw_params_t *hw_params;
+   
 
     if((err = snd_pcm_open(&playback_handle, "default", mode, 0)) < 0)
-    {
-        fprintf(stderr, "cannot open audio device %s (%s)\n", "default",
-                snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot open audio device ") + snd_strerror(err);
 
     if((err = snd_pcm_hw_params_malloc(&hw_params)) < 0)
-    {
-        fprintf(stderr, "cannot allocate hardware parameter structure (%s)\n",
-                snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot allocate hw param. ") + snd_strerror(err);
 
     if((err = snd_pcm_hw_params_any(playback_handle, hw_params)) < 0)
-    {
-        fprintf(stderr, "cannot initialize hardware parameter structure (%s)\n",
-                snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot initialize hw param. ") + snd_strerror(err);
 
     if((err = snd_pcm_hw_params_set_access(playback_handle, hw_params,
                                            SND_PCM_ACCESS_RW_INTERLEAVED)) < 0)
-    {
-        fprintf(stderr, "cannot set access type (%s)\n", snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot set access type ") + snd_strerror(err);
 
     if((err = snd_pcm_hw_params_set_format(playback_handle, hw_params,
                                            SND_PCM_FORMAT_S16_LE)) < 0)
-    {
-        fprintf(stderr, "cannot set sample format (%s)\n", snd_strerror(err));
-        exit(1);
-    }
-    unsigned int actualRate = 44100;
+        throw std::string("cannot set sample format ") + snd_strerror(err);
+
     if((err = snd_pcm_hw_params_set_rate_near(playback_handle, hw_params,
                                               &actualRate, 0)) < 0)
-    {
-        fprintf(stderr, "cannot set sample rate (%s)\n", snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot set sample rate ") + snd_strerror(err);
 
     if((err = snd_pcm_hw_params_set_channels(playback_handle, hw_params, 2)) <
        0)
-    {
-        fprintf(stderr, "cannot set channel count (%s)\n", snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot set channel count ") + snd_strerror(err);
 
     if((err = snd_pcm_hw_params(playback_handle, hw_params)) < 0)
-    {
-        fprintf(stderr, "cannot set parameters (%s)\n", snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot set parameters ") + snd_strerror(err);
 
     snd_pcm_hw_params_free(hw_params);
 
     if((err = snd_pcm_prepare(playback_handle)) < 0)
-    {
-        fprintf(stderr, "cannot prepare audio interface for use (%s)\n",
-                snd_strerror(err));
-        exit(1);
-    }
+        throw std::string("cannot prepare interface ") + snd_strerror(err);
+
     return playback_handle;
 };
 
 int
 main(int argc, char *argv[])
 {
-    int i;
     int err;
-    short buf[128];
-    snd_pcm_t *playback_handle = init(SND_PCM_STREAM_PLAYBACK);
-    snd_pcm_t *capture_handle = init(SND_PCM_STREAM_CAPTURE);
+    short buf[256];
+    unsigned int actualRate = 44100;
+    unsigned int actualPeriod = 1000000/actualRate;
+    snd_pcm_t *capture_handle = init(SND_PCM_STREAM_CAPTURE, actualRate);
+    
 
     try
     {
 
         int nb_ch = 1;
-        lsl::stream_info info_sample("audio", "sample", nb_ch, 0, lsl::cf_int8);
+	int s_chunk = 64;
+        lsl::stream_info info_sample("audio", "sample", nb_ch, 0, lsl::cf_int16);
         lsl::stream_outlet outlet_sample(info_sample);
-        std::vector<char> sample(1);
         std::cout << "[INFOS] Now sending data... " << std::endl;
 
-        for(i = 0; i < 5000; ++i)
+        for(int t=0;;t++)
         {
-            if((err = snd_pcm_readi(capture_handle, buf, 128)) != 128)
-            {
-                fprintf(stderr, "read from audio interface failed (%s)\n",
-                        snd_strerror(err));
-                exit(1);
-            }
-            for(int i = 0; i < 128; i++)
-	      {    
-	       sample[0] =buf[i];
-	       outlet_sample.push_sample(sample);
+            if((err = snd_pcm_readi(capture_handle, buf, s_chunk)) != s_chunk)
+                throw std::string("read from audio  failed ") +
+                    snd_strerror(err);
+	    
+
+            for(int j = 0; j < s_chunk; j++)
+	      {
+                outlet_sample.push_numeric_raw((&buf[2*j]));
+		//usleep(actualPeriod);
 	      }
-            
-            // if((err = snd_pcm_writei(playback_handle, buf, 128)) != 128)
-            // {
-            //     fprintf(stderr, "write to audio interface failed (%s)\n",
-            //             snd_strerror(err));
-            //     exit(1);
-            // }
+	    //std::cout << t << std::endl;
+
         }
     }
     catch(std::exception &e)
@@ -122,6 +88,6 @@ main(int argc, char *argv[])
         std::cerr << "[ERROR] Got an exception: " << e.what() << std::endl;
     }
 
-    snd_pcm_close(playback_handle);
+    snd_pcm_close(capture_handle);
     return 0;
 }
