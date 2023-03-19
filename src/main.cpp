0b2e130c020971c87350b5f1ef582cd454816b7b
aightech <long.devillard@gmail.com>
2023-03-19 19:12:32 +0000

    tested main with lsl audio stream(small delay noticed)

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 #include <lsl_c.h>
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+
 
 double timestamp[1024];
+float data[10240];
+
+int n=0;
 
 /* This routine will be called by the PortAudio engine when audio is needed.
 ** It may called at interrupt level on some machines so don't do anything
@@ -21,7 +27,7 @@ paCallback(const void *inputBuffer,
     float *out = (float *)outputBuffer;
     //pull chunk of data from lsl stream directly into output buffer
     int err;
-    unsigned long n = lsl_pull_chunk_f(*inlet, out, NULL, framesPerBuffer, 0, LSL_FOREVER, &err);
+    lsl_pull_chunk_f(*inlet, out, NULL, framesPerBuffer, 0.08192, 0.0, &err);
     (void)timeInfo;
     (void)statusFlags;
     return 0;
@@ -32,13 +38,16 @@ main(int argc, char *argv[])
 {
     //check if Kistler lsl stream is available
     lsl_streaminfo info;
-    lsl_resolve_byprop(&info, 1, "name", "Kistler", 0, LSL_FOREVER);
-    lsl_inlet inlet = lsl_create_inlet(info, 2, 1024, 1);
+    std::cout << "Searching for stream" << std::endl;
+    if(lsl_resolve_byprop(&info, 1, "type", "measurement", 0, 2)<1)
+        std::cout << "error" << std::endl;
+    lsl_inlet inlet = lsl_create_inlet(info, 1, 1024, 1);
 
     //create audio stream
     AudioStream audio;
-    audio.config(0, 1, 44100, 1024, paFloat32, paCallback, &inlet);
+    audio.config(0, 1, 12500, 1024, paFloat32, paCallback, &inlet);
     audio.start();
+
 
     //wait for user input
     std::cout << "Press enter to stop the audio stream" << std::endl;
